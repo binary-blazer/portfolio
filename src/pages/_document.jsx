7b778805e93af976a4252818265ae308c2b3a40a
JanjyTapYT <81481526+JanjyTapYT@users.noreply.github.com>
2022-10-18 17:07:30 +0200

    Update _document.jsx

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -25,8 +25,10 @@ export default function MyDocument() {
             <link rel="canonical" href={config.siteMetadata.url} />
             <meta name="publisher" content="2022, JanjyTapYT" />
             <meta name="author" content="me@janjytapyt.me" />
-            <link rel="icon" href={config.siteMetadata.favicon} type="image/x-icon" />
             <link href="https://pro.fontawesome.com/releases/v6.0.0-beta1/css/all.css" rel="stylesheet" />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link href="https://fonts.googleapis.com/css2?family=K2D:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
