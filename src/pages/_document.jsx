e7049703c711a916820e4544efdc3e7488ee7bf2
JanjyTapYT <81481526+JanjyTapYT@users.noreply.github.com>
2022-10-18 17:09:58 +0200

    Update _document.jsx

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -18,10 +18,10 @@ export default function MyDocument() {
             <meta property="og:site_name" content={config.siteMetadata.title} />
             <meta property="og:title" content={config.siteMetadata.title} />
             <meta property="og:description" content={config.siteMetadata.description} />
-            <meta property="og:image" content={config.siteMetadata.image} />
             <meta property="og:url" content={config.siteMetadata.url} />
             <meta name="description" content={config.siteMetadata.description} />
             <meta name="theme-color" content={config.siteMetadata.themeColor} />
+            <link rel="icon" href={config.siteMetadata.image} type="image/x-icon" /> 
             <link rel="canonical" href={config.siteMetadata.url} />
             <meta name="publisher" content="2022, JanjyTapYT" />
             <meta name="author" content="me@janjytapyt.me" />
