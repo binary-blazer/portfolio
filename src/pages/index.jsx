6894dabec0a5a9cf4d3912dc49720925fe98947a
JanjyTapYT <janjytapyt@sdevs.org>
2022-11-02 17:17:04 +0100

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -64,7 +64,7 @@ export default function Home() {
                 Here are some of my projects
                 </p>
             <br />
-            <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {_projectsData ? (
                 projects ? (
                     projects?.map((p, i) => (
