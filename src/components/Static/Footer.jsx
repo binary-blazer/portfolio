696baf5a1f2053de499fdfac6502e40202acd58f
JanjyTapYT <janjytapyt@sdevs.org>
2022-11-19 07:44:21 +0100

    Update Footer.jsx

--- src/components/Static/Footer.jsx
+++ src/components/Static/Footer.jsx
@@ -50,14 +50,14 @@ export default function Footer() {
                         </motion.div>
                     ))}
                 </div>
-                 <div className="items-center justify-between gap-4">
+                 <div className="items-center justify-between gap-4 mt-8">
                   <p className="text-1xl text-center text-zinc-400 button-text">© 2020 - {new Date().getFullYear()} {config.siteMetadata.author}. All rights reserved.</p>
                    {/*
                    
                    Dont touch this!
                    
                    */}
-                   <div className="text-center">
+                   <div className="text-center mt-2">
                    <p className="text-lg text-zinc-400 select-none button-text">Made with <i className="fas fa-heart" style={{ color: heartColor, cursor: 'pointer' }} /> by <button onClick={() => router.push('https://janjytapyt.me')} className="text-zinc-400 hover:text-primary hover:dark:text-white button" href="https://janjytapyt.me"><motion.div whileTap={{ scale: 0.95 }}>JanjyTapYT</motion.div></button></p>
                 </div>
                   {/*
