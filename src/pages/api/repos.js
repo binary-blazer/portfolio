452470cec9f4f7b5e54e91812df6c8f244690858
JanjyTapYT <81481526+JanjyTapYT@users.noreply.github.com>
2022-11-21 14:08:32 +0100

    Update repos.js

--- src/pages/api/repos.js
+++ src/pages/api/repos.js
@@ -2,7 +2,7 @@ const config = require('../../../site.config.js');
 
 export default async function handler(req, res) {
     const username = config.github.username
-    const token = "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"
+    const token = process.env.GITHUB_ACCESS_TOKEN
     
     const response = await fetch(
         'https://api.github.com/users/' + username + '/repos?sort=updated&per_page=8',
